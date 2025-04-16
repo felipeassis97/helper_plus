99cff05496157fd3582f5e127a2efd1e18075935
felipeassis97 <felipeassis97@gmail.com>
2025-04-16 15:53:25 +1000

    feat: Initial structure

---
+++ Helper+/HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Helper_App.swift
+//  Helper+
+//
+//  Created by Felipe Assis on 16/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
